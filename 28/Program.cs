bd1c3738a05553faf24e6b3fb493bbbd1293f0ac
Darya <d.a_alpatskaya@mail.ru>
2023-04-04 17:14:24 +0300

    Добавили задачи 24, 26, 28, 30

---
+++ 28/Program.cs
@@ -0,0 +1,26 @@
+﻿//28 Напишите программу, которая принимает на вход число N 
+//и выдаёт произведение чисел от 1 до N.
+
+//4-> 24
+//5-> 120 == 1 * 2 * 3 * 4 * 5
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Произведение цифр = {GetMultiply(num)}");
+
+int GetMultiply(int n)
+{ // n=3
+int result = 1;
+//int i=2;
+// if( n < 0)
+// {
+//       i = n;
+//        n = -1;
+// }
+     for (int i = 2; i <= n; i++)
+     { // i <= n; 2 <= 3
+        result *= i; 
+}
+return result;
+}
---
+++ 30/Program.cs
@@ -0,0 +1,24 @@
+﻿//30 Напишите программу, которая выводит массив из 8 элементов, 
+//заполненный нулями и единицами в случайном порядке.
+
+//[1,0,1,1,0,1,0,0]
+// int a = new Random().Next(2, 7)
+// int b = new Random().Next(2)
+// Console.WriteLine($"a = {a},b = {b}");
+
+int[] array = GetBinArray(8);
+Console.WriteLine($"[ {String.Join(", ", array)} ]");
+
+// for (int i = 0; i < array.Lenght - 1; i++){
+//   Console.Write($"{array[i]}, ");
+//}
+// Console.Write($"{array[array.Lenght - 1]}]");
+
+int[] GetBinArray(int size){
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(2);
+    }
+    return result;
+}
